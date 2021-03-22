05dbe12a5006523db70625a9acea7b51906cece8
chriss <13677134+devcshort@users.noreply.github.com>
2021-03-22 14:26:18 -0700

    UPdating gitignore

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export declare const Theme: import("@material-ui/core").Theme;
-declare type ThemeProviderProps = {
-    children: React.ReactNode;
-};
-export declare const ThemeProvider: ({ children }: ThemeProviderProps) => JSX.Element;
-export {};
